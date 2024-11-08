5204c76df27d5cbc7d4400c42add913c1ef95bad
Herby Fleurentin <fleurentinherby7@yahoo.fr>
2024-11-08 23:02:50 +0100

    Ajout de l'animation du texte sur la page d'accueil

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [text, setText] = useState('');
+  const fullText = "Bonjour, je m'appelle Herby Fleurentin, je suis cartographe et géomaticien.";
+
+  useEffect(() => {
+    let i = 0;
+    const intervalId = setInterval(() => {
+      setText((prevText) => prevText + fullText[i]);
+      i += 1;
+      if (i === fullText.length) {
+        clearInterval(intervalId);
+      }
+    }, 100); // délai de 100ms entre chaque lettre
+    return () => clearInterval(intervalId); // nettoyer l'intervalle quand le composant est démonté
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1 className="animated-text">Bonjour, je m'appelle Herby, Ingénieur Géomaticien.</h1>
-      </header>
+      <h1>{text}</h1>
     </div>
   );
 }
